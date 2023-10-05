e53e91031c5ce80610910ef15218067e0f4cf2fd
Ankit Mittal <ankitmittal0814@gmail.com>
2023-10-05 22:57:19 +0530

    Update App.jsx

--- 06passwordGenerator/src/App.jsx
+++ 06passwordGenerator/src/App.jsx
@@ -37,74 +37,7 @@ function App() {
     }, 85);
   }, [password]);
 
-  return (
-    <>
-      <div className="w-full mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 text-2xl box mt-20">
-        <h1 className="text-white text-center my-7 mt-4 text-3xl">
-          Password Generator
-        </h1>
-        <div className="flex shadow rounded-lg overflow-hidden mb-9">
-          <input
-            type="text"
-            value={password}
-            className="outline-none w-full py-1 px-3"
-            placeholder="Password"
-            readOnly
-            ref={passwordRef}
-          />
-          <button
-            onClick={copyPasswordToClipboard}
-            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 pb-2 "
-            style={{ backgroundColor: btnColor }}
-          >
-            copy
-          </button>
-        </div>
-
-        <div className="flex text-sm gap-x-2">
-          <div className="flex items-center gap-x-1 text-2xl mr-9">
-            <input
-              type="range"
-              min={8}
-              max={100}
-              value={length}
-              className="cursor-pointer"
-              ref={passwordRef}
-              onChange={(e) => {
-                setLength(e.target.value);
-              }}
-            />
-            <label>Length:{length}</label>
-          </div>
-
-          <div className="flex items-center gap-x-1 text-2xl mr-9">
-            <input
-              type="checkbox"
-              defaultChecked={numAllowed}
-              id="numberInput"
-              className="cursor-pointer w-5 h-5"
-              onChange={() => {
-                setNumAllowed((prev) => !prev);
-              }}
-            />
-            <label>Number</label>
-          </div>
-          <div className="flex items-center gap-x-1 text-2xl">
-            <input
-              type="checkbox"
-              defaultChecked={charAllowed}
-              id="charInput"
-              className="cursor-pointer w-5 h-5"
-              onChange={() => {
-                setCharAllowed((prev) => !prev);
-              }}
-            />
-            <label>Character</label>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
