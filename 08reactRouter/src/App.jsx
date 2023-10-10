c9ddcfe7a0611288ac76815794a5e7b42daaef73
Ankit Mittal <ankitmittal0814@gmail.com>
2023-10-10 12:55:24 +0530

    Update logo

--- 08reactRouter/src/App.jsx
+++ 08reactRouter/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <Footer />
       <h1 className="bg-green-500 p-5 text-center">React Router</h1>
     </>
   );
