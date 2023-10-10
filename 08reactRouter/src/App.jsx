ac5d185de223bab3f1efba5a6616dbeb4d5714b5
Ankit Mittal <ankitmittal0814@gmail.com>
2023-10-10 12:42:30 +0530

    Add Header

--- 08reactRouter/src/App.jsx
+++ 08reactRouter/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <h1 className="bg-green-500 p-5 text-center">React Router</h1>
     </>
   );
