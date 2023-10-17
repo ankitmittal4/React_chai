913160b0af5eb5a0fcd98f36eb15a7b6c19f1ce7
Ankit Mittal <ankitmittal0814@gmail.com>
2023-10-18 01:05:17 +0530

    Update react router

---
+++ 08reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Github() {
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      GitHub Followers:
+    </div>
+  );
+}
+
+export default Github;
+GitHub;
