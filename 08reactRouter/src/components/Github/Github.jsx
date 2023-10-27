bd3285987f133ac8ff222b5a89eb8801a286a3d9
Ankit Mittal <ankitmittal0814@gmail.com>
2023-10-27 12:19:40 +0530

    context Api

--- 08reactRouter/src/components/Github/Github.jsx
+++ 08reactRouter/src/components/Github/Github.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Github() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/hiteshchoudhary")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
-      GitHub Followers:
+      GitHub Followers: {data.followers}
+      <img src={data.avatar_url} alt="Git Picture" width={300}></img>
     </div>
   );
 }
 
 export default Github;
-GitHub;
