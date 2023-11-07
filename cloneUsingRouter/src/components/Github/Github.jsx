d72f04afaf7503d3870d2e50d60177b3b746854b
Ankit <ankitmittal0814@gmail.com>
2023-11-07 19:19:35 +0530

    Clone using Router

---
+++ cloneUsingRouter/src/components/Github/Github.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+function Github() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/ankitmittal4")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setData(data);
+      });
+  });
+  return (
+    <div className="text-4xl bg-gray-600 text-center m-5 py-5 text-white">
+      <p className="mb-3">Username: {data.login}</p>
+      <p className="mb-3">Followers: {data.followers}</p>
+      <p className="mb-3">Location: {data.location}</p>
+
+      <div className="h-screen flex items-center justify-center">
+        <img src={data.avatar_url} alt="Github Avatar" />
+      </div>
+    </div>
+  );
+}
+
+export default Github;
