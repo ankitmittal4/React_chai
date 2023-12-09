3ae9274961394ecbc97f115fd6df31bb269fbee3
Ankit <ankitmittal0814@gmail.com>
2023-12-09 12:43:22 +0530

    Redux toolkit

--- 12reduxToolkitTodo/src/App.jsx
+++ 12reduxToolkitTodo/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import reacLogo from "/vite.svg";
 import "./App.css";
-
+import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Redux Toolkit</h1>
+      <AddTodo />
+      <Todo />
     </>
   );
 }
